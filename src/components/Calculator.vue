58ae10c742f111c97a4b38777dd985e518b41dd7
Jengle <jennyrob282@gmail.com>
2019-03-21 20:26:37 +0100

    first iteration done

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="main">
     <h1>KALKULATOR</h1>
+    <p>{{fact}}</p>
     <div class="calculator">
-      <div class="btn display">{{current || 0}}</div>
+      <div :style="styleObject" class="component display">{{display || 0}}</div>
       <br>
-      <div @click="clear" class="btn clear">C</div>
-      <div class="btn input">+/-</div>
-      <div @click="append(`/`)" class="btn operator">/</div>
-      <div @click="append(7)" class="btn input">7</div>
-      <div @click="append(8)" class="btn input">8</div>
-      <div @click="append(9)" class="btn input">9</div>
-      <div @click="append(`*`)" class="btn operator">x</div>
-      <div @click="append(4)" class="btn input">4</div>
-      <div @click="append(5)" class="btn input">5</div>
-      <div @click="append(6)" class="btn input">6</div>
-      <div @click="append(`-`)" class="btn operator">-</div>
-      <div @click="append(1)" class="btn input">1</div>
-      <div @click="append(2)" class="btn input">2</div>
-      <div @click="append(3)" class="btn input">3</div>
-      <div @click="append(`+`)" class="btn operator">+</div>
-      <div @click="append(0)" class="btn input">0</div>
-      <div @click="append(`.`)" class="btn input">.</div>
-      <div @click="calculate" class="btn operator equals">=</div>
+      <div @click="clear" class="component btn clear">K</div>
+      <div @click="plusMinus" class="component btn input">+/-</div>
+      <div @click="operation(`/`)" class="component btn operator">/</div>
+      <div @click="input(`7`)" class="component btn test input">7</div>
+      <div @click="input(`8`)" class="component btn input">8</div>
+      <div @click="input(`9`)" class="component btn input">9</div>
+      <div @click="operation(`*`)" class="component btn operator">x</div>
+      <div @click="input(`4`)" class="component btn input">4</div>
+      <div @click="input(`5`)" class="component btn input">5</div>
+      <div @click="input(`6`)" class="component btn input">6</div>
+      <div @click="operation(`-`)" class="component btn operator">-</div>
+      <div @click="input(`1`)" class="component btn input">1</div>
+      <div @click="input(`2`)" class="component btn input">2</div>
+      <div @click="input(`3`)" class="component btn input">3</div>
+      <div @click="operation(`+`)" class="component btn operator">+</div>
+      <div @click="input(`0`)" class="component btn input">0</div>
+      <div @click="input(`.`)" class="component btn input">.</div>
+      <div @click="operation(`start`)" class="component btn operator equals">=</div>
     </div>
   </div>
 </template>
@@ -30,30 +31,67 @@
 export default {
   data() {
     return {
-      current: ""
+      runningCalc: "", // the running result of calculations performed
+      display: "", // what is shown in the display div
+      lastBtnType: "start", //  to determine logic for input and operator method can be "start" "input" or "operator"
+      operator: "", // the last operator button clicked
+      fact: "return a result to see a number fakt", // fact relating to the result from numbers api
+      styleObject: {
+        direction: "ltr"
+      } // when display is too long for the screen can change language direction and clip on the left
     };
   },
+
   methods: {
     clear() {
-      this.current = "";
-    },
-    clearAll() {
-      this.accumulator = "";
-      this.current = "";
-    },
-    append(value) {
-      this.current += value;
-      // maximum length
-      // can't put zero at teh front
+      this.runningCalc = "";
+      this.display = "";
+      this.lastBtnType = "start";
+      this.operator = "";
+      this.fact = "Return a result to see a number fakt";
     },
 
     plusMinus() {
-      // don't need if showing string in array
-      this.current = `-${this.current}`;
+      this.display = String(parseInt(this.display) * -1);
     },
 
-    calculate() {
-      this.current = eval(this.current);
+    input(value) {
+      if (this.lastBtnType !== "input" && value != 0) {
+        this.display = value;
+      } else if (this.lastBtnType === "input" && this.display !== 0) {
+        this.display += value;
+      }
+      if (this.display.length > 16) this.styleObject.direction = "rtl";
+      this.lastBtnType = "input";
+    },
+
+    operation(value) {
+      if (this.operator === "start") {
+        this.runningCalc = this.display;
+      } else {
+        this.runningCalc = eval(
+          `${this.runningCalc}${this.operator}${this.display}`
+        );
+      }
+      if (value === "start") this.getFact(this.runningCalc);
+      if (this.display.length > 16) {
+        // this.runningCalc = String(parseInt(this.display).toExponential());
+        this.styleObject.direction = "ltr";
+      }
+      this.display = this.runningCalc;
+      this.operator = value;
+      this.lastBtnType = "operator";
+    },
+
+    getFact(number) {
+      number = Math.round(number) || 0;
+      axios
+        .get(`http://numbersapi.com/${number}?notfound=floor`)
+        .then(fact => {
+          console.log(fact);
+          this.fact = fact.data;
+        })
+        .catch(error => console.log(error));
     }
   }
 };
@@ -68,17 +106,30 @@ export default {
   border-radius: 18px;
 }
 
+.component {
+  display: inline-block;
+  border-radius: 18px;
+  cursor: default;
+  user-select: none;
+}
+
 .btn {
   width: 80px;
   height: 80px;
-  display: inline-block;
   margin: 6px;
-  border-radius: 18px;
   line-height: 80px;
   font-size: 28px;
+  box-shadow: 2px 2px rgb(75, 74, 74);
+}
+
+.btn:active {
+  box-shadow: 1px 1px rgb(99, 98, 98);
+  transform: translateY(1px);
+  transform: translateX(1px);
 }
 
 .display {
+  box-shadow: 0 0;
   width: 85%;
   height: 60px;
   padding: 5px;
@@ -88,13 +139,17 @@ export default {
   margin-bottom: 15px;
   font-size: 34px;
   background-color: white;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 }
+
 .equals {
   width: 172px;
 }
 
 .operator {
-  background-color: rgb(65, 58, 47);
+  background-color: rgb(104, 103, 102);
   color: white;
 }
 
