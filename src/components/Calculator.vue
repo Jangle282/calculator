cf6afcfbee72605ce1c5d4f31895f338c6c8da84
Jengle <jennyrob282@gmail.com>
2019-03-23 21:05:48 +0100

    key events, sounds and showing operator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,12 +3,15 @@
     <h1>KALKULATOR</h1>
     <p>{{fact}}</p>
     <div class="calculator">
-      <div :style="styleObject" class="component display">{{display || 0}}</div>
+      <div :style="styleObject" class="component display">
+        <p>{{operator}}</p>
+        {{display || 0}}
+      </div>
       <br>
       <div @click="clear" class="component btn clear">C</div>
       <div @click="plusMinus" class="component btn input">+/-</div>
       <div @click="operation(`/`)" class="component btn operator">/</div>
-      <div @click="input(`7`)" class="component btn test input">7</div>
+      <div @click="input(`7`)" class="component btn input">7</div>
       <div @click="input(`8`)" class="component btn input">8</div>
       <div @click="input(`9`)" class="component btn input">9</div>
       <div @click="operation(`*`)" class="component btn operator">x</div>
@@ -22,7 +25,7 @@
       <div @click="operation(`+`)" class="component btn operator">+</div>
       <div @click="input(`0`)" class="component btn input">0</div>
       <div @click="input(`.`)" class="component btn input">.</div>
-      <div @click="operation(`start`)" class="component btn operator equals">=</div>
+      <div @click="operation(`ans`)" class="component btn operator equals">=</div>
     </div>
   </div>
 </template>
@@ -31,33 +34,39 @@
 export default {
   data() {
     return {
-      runningCalc: "", // the running result of calculations performed
+      runningCalc: "", // the running result of calculations performed inbetween clears or new anss.
       display: "", // what is shown in the display div
-      lastBtnType: "start", //  to determine logic for input and operator method can be "start" "input" or "operator"
+      lastBtnType: "ans", //  to determine logic for input and operator method can be "ans" "input" or "operator"
       operator: "", // the last operator button clicked
-      fact: "return a result to see a number fact", // fact relating to the result from numbers api
+      fact: "Return a result to see a number fact", // fact relating to the result from numbers api
       styleObject: {
         direction: "ltr"
       } // when display is too long for the screen can change language direction and clip on the left
     };
   },
 
+  created: function() {
+    window.addEventListener("keydown", this.keyDown);
+  },
+
   methods: {
-    // resets all data when clear button pressed
     clear() {
+      this.keySound();
       this.runningCalc = "";
       this.display = "";
-      this.lastBtnType = "start";
+      this.lastBtnType = "ans";
       this.operator = "";
       this.fact = "Return a result to see a number fact";
     },
 
     plusMinus() {
+      this.keySound();
       this.display = String(parseFloat(this.display) * -1);
       this.displayDir();
     },
 
     input(value) {
+      this.keySound();
       if (this.lastBtnType !== "input" && value != 0) {
         this.display = value;
       } else if (this.lastBtnType === "input" && this.display !== 0) {
@@ -69,17 +78,18 @@ export default {
     },
 
     operation(value) {
-      if (this.operator === "start") {
-        this.runningCalc = this.display; // stores the first value for running calc.
+      this.keySound();
+      if (this.operator === "ans") {
+        this.runningCalc = this.display; // for the first operator button press after a clear
       } else if (this.lastBtnType === "operator") {
-        this.operator = value; // if operator pushed more than once in a row - doesn't calculate anything or change display value
+        this.operator = value; // if an operator is pushed more than once in a row - only changes the next operation to be performed.
       } else {
         this.runningCalc = eval(
           `${this.runningCalc}${this.operator}${this.display}`
         ); // calculates the running total according to the previous operator clicked and stores to runningCalc
       }
       this.display = this.runningCalc; // displays the runningCalc
-      if (value === "start") this.getFact(this.runningCalc); // retreives number fact for the displayed value if the button pressed was the equals button
+      if (value === "ans") this.getFact(this.runningCalc); // retreives number fact for the displayed value if the button pressed was the equals button
       this.displayDir();
       this.operator = value; // sets the operator to the one the user just clicked
       this.lastBtnType = "operator"; //
@@ -90,12 +100,11 @@ export default {
       if (this.display && this.display.length > 16) {
         this.display = String(parseFloat(this.display).toExponential());
         this.styleObject.direction = "rtl";
-        console.log("hello from if", this.styleObject.direction);
       } else {
         this.styleObject.direction = "ltr";
-        console.log("hello from elese", this.styleObject.direction);
       }
     },
+
     // API call for number fact
     getFact(number) {
       number = Math.round(number) || 0;
@@ -105,6 +114,51 @@ export default {
           this.fact = fact.data;
         })
         .catch(error => console.log(error));
+    },
+
+    // keyboard inputs
+    keyDown(e) {
+      if (this.operator === "ans" && e.key === "Backspace") this.clear();
+      else {
+        switch (e.key) {
+          case "Enter":
+            this.operation(`ans`);
+            break;
+          case "Backspace":
+            this.display = this.display.slice(0, -1);
+            break;
+          case "1":
+          case "2":
+          case "3":
+          case "4":
+          case "5":
+          case "6":
+          case "7":
+          case "8":
+          case "9":
+          case "0":
+            this.input(e.key);
+            break;
+          case ".":
+          case "=":
+          case "+":
+          case "*":
+          case "/":
+          case "-":
+            this.operation(e.key);
+            break;
+          case "Delete":
+          case "C":
+          case "c":
+            this.clear();
+            break;
+        }
+      }
+    },
+
+    keySound() {
+      var keyPress = new Audio("keyPress.mp3");
+      keyPress.play();
     }
   }
 };
@@ -132,7 +186,7 @@ export default {
   margin: 6px;
   line-height: 80px;
   font-size: 28px;
-  box-shadow: 2px 2px rgb(75, 74, 74);
+  box-shadow: 3px 3px rgb(75, 74, 74);
 }
 
 .btn:active {
@@ -141,12 +195,18 @@ export default {
   transform: translateX(1px);
 }
 
+p {
+  font-size: 14px;
+  line-height: 0px;
+  padding: 0;
+}
+
 .display {
   box-shadow: 0 0;
   width: 85%;
-  height: 60px;
+  height: 70px;
   padding: 5px;
-  line-height: 60px;
+  line-height: 40px;
   text-align: right;
   padding-right: 20px;
   margin-bottom: 15px;
